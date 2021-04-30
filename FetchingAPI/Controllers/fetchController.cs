8c32bd629847feca4ab62adb8e2e4764a2f234bd
kankan-dev <83448171+kankan-dev@users.noreply.github.com>
2021-04-30 15:02:04 +0530

    API

---
+++ FetchingAPI/Controllers/fetchController.cs
@@ -0,0 +1,40 @@
+﻿using FetchingAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace FetchingAPI.Controllers
+{
+    [Route("api/fetch")]
+    [ApiController]
+    public class fetchController : ControllerBase
+    {
+        [HttpGet]
+        [Route("data")]
+
+        public  IActionResult GetData()
+        {
+            try 
+            {
+                var client = new WebClient();
+                var json = client.DownloadString("https://api.coindesk.com/v1/bpi/currentprice.json");
+                var result = JsonConvert.DeserializeObject<Coindesk>(json);
+                return Ok(result);
+            }
+            catch(Exception e)
+            {
+                return BadRequest(e.Message + " " + e.StackTrace);
+            }
+            
+
+        }
+
+
+
+    }
+}
---
+++ FetchingAPI/Models/Coindesk.cs
@@ -0,0 +1,68 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FetchingAPI.Models
+{
+        public partial class Coindesk
+        {
+            [JsonProperty("time")]
+            public Time Time { get; set; }
+
+            [JsonProperty("disclaimer")]
+            public string Disclaimer { get; set; }
+
+            [JsonProperty("chartName")]
+            public string ChartName { get; set; }
+
+            [JsonProperty("bpi")]
+            public Bpi Bpi { get; set; }
+        }
+
+        public partial class Bpi
+        {
+            [JsonProperty("USD")]
+            public Details Usd { get; set; }
+
+            [JsonProperty("GBP")]
+            public Details Gbp { get; set; }
+
+            [JsonProperty("EUR")]
+            public Details Eur { get; set; }
+        }
+
+        public partial class Details
+        {
+            [JsonProperty("code")]
+            public string Code { get; set; }
+
+            [JsonProperty("symbol")]
+            public string Symbol { get; set; }
+
+            [JsonProperty("rate")]
+            public string Rate { get; set; }
+
+            [JsonProperty("description")]
+            public string Description { get; set; }
+
+            [JsonProperty("rate_float")]
+            public double RateFloat { get; set; }
+        }
+
+        public partial class Time
+        {
+            [JsonProperty("updated")]
+            public string Updated { get; set; }
+
+            [JsonProperty("updatedISO")]
+            public DateTimeOffset UpdatedIso { get; set; }
+
+            [JsonProperty("updateduk")]
+            public string Updateduk { get; set; }
+        }
+    
+
+
+}
